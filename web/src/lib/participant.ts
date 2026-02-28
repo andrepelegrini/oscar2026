4819fa8156e8fc2231c41a8a8b456ec86ddd4c99
Andre Pelegrini <pelegriniandre@Andres-MacBook-Pro.local>
2026-02-27 23:29:48 -0300

    fix participant name

--- web/src/lib/participant.ts
+++ web/src/lib/participant.ts
@@ -1,8 +1,8 @@
 export function getParticipantId() {
     if (typeof window === "undefined") return null;
-    return localStorage.getItem("participant_id");
+    return localStorage.getItem("user_id");
   }
   
   export function setParticipantId(id: string) {
-    localStorage.setItem("participant_id", id);
+    localStorage.setItem("user_id", id);
   }
